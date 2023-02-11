15038a288051c13811fc84aa458d9350755c2a72
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-02-11 16:58:30 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let editbutton = document.querySelector('.profile__button_edit');
+let editbutton = document.querySelector('.profile__button-edit');
 let popup = document.querySelector(".popup");
-let closePopup = document.querySelector(".popup__button_close");
-let inputName = document.querySelector(".popup__input_name");
-let inputJob = document.querySelector(".popup__input_job");
-let placeName = document.querySelector(".profile__person_name");
-let placeJob = document.querySelector(".profile__person_job");
-let saveForm = document.querySelector(".popup__button_save");
+let closePopup = document.querySelector(".popup__button-close");
+let inputName = document.querySelector(".popup__input-name");
+let inputJob = document.querySelector(".popup__input-job");
+let placeName = document.querySelector(".profile__person-name");
+let placeJob = document.querySelector(".profile__person-job");
+let saveForm = document.querySelector(".popup__button-save");
 let profile = document.querySelector(".profile");
 let container = document.querySelector(".container");
-let likeButton = document.querySelector(".card__caption_like-button");
+let likeButton = document.querySelector(".card__caption-like");
 
     function putAlike() {
         likeButton.style.setProperty('background-image', 'url("./images/Activelike.svg")')
@@ -35,11 +35,11 @@ closePopup.addEventListener('click', closePopUp);
     function inputNameAndJob() {
         profile.innerHTML = `
             <img class="profile__avatar" src="./images/avatar.jpg" alt="аватар">
-            <h1 class="profile__person_name"> ${inputName.value}
-            <input type="button" class="profile__button_edit">
+            <h1 class="profile__person-name"> ${inputName.value}
+            <input type="button" class="profile__button-edit">
             </h1>
-            <button class="profile__button_add-content">+</button>
-            <p class="profile__person_job">${inputJob.value}</p> 
+            <button class="profile__button-add">+</button>
+            <p class="profile__person-job">${inputJob.value}</p> 
         `;
         inputName.value = '';
         inputJob.value = '';
