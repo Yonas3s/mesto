8738b1b344a7c5a392980b682667a559a8ea2361
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-02-16 20:24:46 +0300

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,49 @@
+let editbutton = document.querySelector('.profile__button-edit');
+let popup = document.querySelector(".popup");
+let closePopup = document.querySelector(".popup__button-close");
+let inputName = document.querySelector(".popup__input-name");
+let inputJob = document.querySelector(".popup__input-job");
+let placeName = document.querySelector(".profile__person-name");
+let placeJob = document.querySelector(".profile__person-job");
+let saveForm = document.querySelector(".popup__button-save");
+let profile = document.querySelector(".profile");
+let container = document.querySelector(".container");
+let likeButton = document.querySelector(".card__caption-like");
+
+    function putAlike() {
+        likeButton.style.setProperty('background-image', 'url("./images/Activelike.svg")')
+    }
+
+likeButton.addEventListener('click', putAlike);
+
+    function openPopUp() {
+        popup.classList.add("visible"); 
+        closePopup.classList.add("visible");
+        container.style.setProperty('opacity', '0.5');
+    }
+
+editbutton.addEventListener('click', openPopUp);
+
+    function closePopUp() {
+        popup.classList.remove("visible"); 
+        closePopup.classList.remove("visible");
+        container.style.setProperty('opacity', '1');
+    }
+
+closePopup.addEventListener('click', closePopUp);
+
+    function inputNameAndJob() {
+        profile.innerHTML = `
+            <img class="profile__avatar" src="./images/avatar.jpg" alt="аватар">
+            <h1 class="profile__person-name"> ${inputName.value}
+            <input type="button" class="profile__button-edit">
+            </h1>
+            <button class="profile__button-add">+</button>
+            <p class="profile__person-job">${inputJob.value}</p> 
+        `;
+        inputName.value = '';
+        inputJob.value = '';
+        closePopUp();
+    }
+
+saveForm.addEventListener('click', inputNameAndJob);
